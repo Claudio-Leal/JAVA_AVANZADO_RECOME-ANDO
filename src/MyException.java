997507c710be778c8bfee1559f269c24210d09dd
claud <claud@LAPTOP-BQ13H4NF>
2020-05-30 20:31:27 -0300

    Versão 3 - criar uma exceção em java - 1

---
+++ src/MyException.java
@@ -0,0 +1,18 @@
+
+/*
+ * Abaixo, a classe Minha exceção criada, sendo extendida para a classe "interna",...
+ * Exception. Com isso, já vimos que todos os métodos da classe Exception serão carregados aqui.
+ * Estrutura desta classe:
+ * public: poderá ser chamada de qualquer local da aplicação.
+ * class: indicação de que a declaração é uma classe.
+ * extends: extende os métodos da classe "Exception" a esta classe.
+ * Exption: classe "interna que guarda métodos de exceção. 
+ */
+
+
+public class MyException extends Exception{
+	
+	//Declaração de um CONSTRUCTOR METHOD:
+	public MyException 
+
+}
