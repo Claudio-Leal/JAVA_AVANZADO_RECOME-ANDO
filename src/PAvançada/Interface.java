6436d951d420465fa06323648fe2eafb65c296ac
Cláudio Leal <claudiolealti@gmail.com>
2020-06-01 10:55:55 -0300

    Versão 4 - registros importantes sobre a implementação de interface

--- src/PAvançada/Interface.java
+++ src/PAvançada/Interface.java
@@ -1,13 +1,34 @@
 /*
- * Abaixo, a estrutura básica de uma interface:
  * Nela consta:
- * 
- *  
  * A palavra INTERFACE é que determina que este "arquivo" se trata de uma interface. 
  */
 
 package PAvançada;
 
 public interface Interface {
-
+	// INTERFACE: possui método com classe não implementada. 
+	public void metodo ();
 }
+	
+	/*
+	 *
+	 * 
+	 * Acima, temos método "sem bloco", ou seja, sem {}.
+	 * public: poderá ser chamada de qualquer parte da aplicação.
+	 * void: retorna vazio. 
+	 * (): parâmetros,...argumentos, sem nenhum parâmetro definido. 
+	 * Trata-se de um MÉTODO SEM CÓDIGO. 
+	 * se observarmos a classe, nela consta um sout para imprimir a letra A. 
+	 * Esta possibilidade não existe aqui, já que estamos trabalhando um MÉTODO SEM CÓDIGO.
+	 * 
+	 * Se tentarmos a declaração abaixo, não vai funcionar,...o sistema apresentará erro,
+	 * pois aqui se trata de uma interface. 
+	 * 
+	 *  public void metodo () {		
+	 }
+	 *
+	 *
+	 * A regra é: se estivermos tratando de uma INTERFACE, os métodos não
+	 * poderão ser implementados. 
+	 *
+	 */
