97be2d0eeaeda8415de4f29e48189426893f3e95
Cláudio Leal <claudiolealti@gmail.com>
2020-06-01 12:40:11 -0300

    Versão 9 - Exemplos práticos

---
+++ src/Exemplo/Estadio.java
@@ -0,0 +1,15 @@
+/*
+ * Criação da interface Estádio. 
+ * Vamos inserir dois métodos sem implementação, como vimos anteriormente.
+ * A criação destes dois métodos serão a base de tudo que será executado daqui em diante.  
+ */
+
+package Exemplo;
+
+// INTERFACE
+public interface Estadio {
+	
+	// DOIS MÉTODOS (SEM BLOCO)
+	public void abrirEstadio();
+	public void fecharEstadio();
+}
