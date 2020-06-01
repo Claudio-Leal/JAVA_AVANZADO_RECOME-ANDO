3409f054ae703669957ef5b8d170c7dfbc76443c
Cláudio Leal <claudiolealti@gmail.com>
2020-06-01 19:22:01 -0300

    Versão 14

--- src/Exemplo/MainI.java
+++ src/Exemplo/MainI.java
@@ -13,8 +13,11 @@ public class MainI {
 	public static void main (String [] argumentos) {
 			// EstadioMundial é uma classe, que cria o objeto classe...
 			// e que tenta instanciar EstadioMundial. 
+			// Infelizmente, o linkedin não mostra uma situação como esta,...
+			// ou seja, o erro que aparece no objeto classe. 
+			// Vou seguir com o curso,...é possível que a resposta apareça mais na frente. 
 			EstadioMundial classe = new EstadioMundial();
 		}
-
+}
 
 
