fb708f9b0f28b2044cacedf4ee76f24333ce447f
Cláudio Leal <claudiolealti@gmail.com>
2020-06-01 11:48:02 -0300

    Versão 5 - Correções na Classe

--- src/PAvançada/Classe.java
+++ src/PAvançada/Classe.java
@@ -1,21 +1,21 @@
 /*
  * Abaixo, temos a estrutura básica de uma classe:
  * Nela consta:
- * MÉTODO CONSTRUTOR
  * ATRIBUTO (VARIÁVEL)
+ * CONSTRUTOR  
  * MÉTODO 
  * A palavra CLASS é que determina que este "arquivo" se trata de uma classe. 
  */
 
 package PAvançada;
 
-// MÉTODO CONSTRUTOR
+
 public class Classe {
-	
-	
 	// ATRIBUTO
 	int numeros;
-	
+	// CONSTRUTOR
+	public Classe () {		
+	}
 	// MÉTODO
 	public void metodo () {	
 		System.out.println("A");
---
+++ src/PAvançada/ClasseAbstrata.java
@@ -0,0 +1,27 @@
+/*
+ * Inicialmente, vamos colocar todos os elementos comuns de uma classe.
+ * Primeira regra: a palavra ABSTRACT terá que aparecer na declaração 
+ * da classe. Se a palavra ABSTRACT não existir, será uma classe comum.
+ * 
+ * É requisito de uma CLASSE ABSTRATA, a criação de um método ABSTRATO (está no final do código). 
+ * 
+ */
+
+
+package PAvançada;
+
+
+public abstract class ClasseAbstrata {	
+
+	int numeros;
+	
+
+
+public void metodo () {	
+}
+
+
+
+public abstract void correr ()
+
+}
