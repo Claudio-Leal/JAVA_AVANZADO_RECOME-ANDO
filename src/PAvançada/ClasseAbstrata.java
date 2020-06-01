f82fece3375026ecac4d4f3661138e28afde7960
Cláudio Leal <claudiolealti@gmail.com>
2020-06-01 12:33:40 -0300

    Versão 8 - com definições precisas sobre os três arquivos.

--- src/PAvançada/ClasseAbstrata.java
+++ src/PAvançada/ClasseAbstrata.java
@@ -1,7 +1,12 @@
 /*
  * Inicialmente, vamos colocar todos os elementos comuns de uma classe.
  * Primeira regra: a palavra ABSTRACT terá que aparecer na declaração da classe. 
- * Se a palavra ABSTRACT não existir, estaremos tratando de uma classe comum. 
+ * Se a palavra ABSTRACT não existir, estaremos tratando de uma classe comum.
+ * No fim do código, vamos criar um novo MÉTODO ABSTRATO chamado CORRER, que também não terá bloco de código.
+ * Se a palavra abstract não existir, o sistema aponta um erro. 
+ * Classe abstrata precisa ter pelo menos um método sem implementação. Se tivermos mais de um método,
+ * pelo menos um deles não poderá ter implementação. 
+ * Em outras palavras, ao menos um método precisa estar "vazio", senão não se trata de uma classe abstrata.   
  */
 package PAvançada;
 
@@ -12,7 +17,6 @@ public abstract class ClasseAbstrata {
 	// CONSTRUTOR
 	public ClasseAbstrata () {	
 	}
-	// MÉTODO
-	public void correr () {
-	}
+	// MÉTODO ABSTRATO (SEM BLOCO)
+	public abstract void correr ();
 }
