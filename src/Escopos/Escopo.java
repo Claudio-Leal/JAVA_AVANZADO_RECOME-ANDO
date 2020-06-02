1a77568337eaee56dcce7e5aba51a3c1ca41a4f0
Cláudio Leal <claudiolealti@gmail.com>
2020-06-02 17:12:54 -0300

    Versão 2 - Escopo (com comentários finais)

--- src/Escopos/Escopo.java
+++ src/Escopos/Escopo.java
@@ -21,5 +21,29 @@ public class Escopo {
 	// Método 2
 	public void metodo2 ( ) {		
 	}	
+	
+	
+	// Método 3
+	// estamos retornando para o método o valor de "dado". 
+	int somar () {		
+		// as variáveis declaradas aqui são específicas do método somar,..
+		// ou seja, estão no escopo do método somar.
+		// não poderão ser acessadas "de fora" do método. 
+		int dado = 0;
+		return dado;
+		}
+	
+	
+	// Método 4
+	public void metodo4 () {
+		for (int i = 0 ; i < 10 ; i++) {
+			int h = 20;
+			h = 90;
+		}
+		
+		// atribuição abaixo comentada porque a variável h está dentro do escopo...
+		// do for do "metodo4",...então não pode ser acessada de fora do escopo.		
+		// h = 90 -- > foi posicionado dentro do escopo do for. 
+	}	
 }
 //--------------------------------Fim do escopo
