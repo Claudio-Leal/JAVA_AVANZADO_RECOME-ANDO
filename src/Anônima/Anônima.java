7ab551570891c68ffcf1152d281989c8b531d7d2
Cláudio Leal <claudiolealti@gmail.com>
2020-06-02 17:42:34 -0300

    Versão 1 - Classe anônima

---
+++ src/Anônima/Anônima.java
@@ -0,0 +1,31 @@
+/*
+ * ...e é no bloco do objeto "anonima" que a definição desta ação,
+ * ou seja, um simples clique de um botão, etc., será definido.
+ * importante frisar que este código não foi executado. É apenas conceitual. 
+ * E o corpo da classe anonima também não foi definido.  
+ */
+
+
+package Anônima;
+public class Anônima {
+	int numero;
+	String dadoString;
+	
+	public static void main (String [] args) {
+		// Apenas para registrar: esta é a sintaxe normalmente utilizada para...
+		// criação de objetos. 
+		// Object objeto = new Object();
+		
+		// Utilização da interface Caminhar para criar o objeto anonima.
+		Caminhar anonima = new Caminhar() {
+
+			// Corpo do ojbeto é inserido automaticamente.
+			// O interessante é que ele já busca o método "avançar", existente...
+			// dentro da interface Caminhar, para que possamos implementá-lo. 
+			@Override
+			public void avançar() {
+				// TODO Auto-generated method stub				
+			}
+		};		
+	}
+}
---
+++ src/Anônima/Caminhar.java
@@ -0,0 +1,17 @@
+/*
+ * Interface criada para entendermos o conceito de classe anônima.
+ * A verdade é que não tem muito o que falar sobre classe anônima.
+ * o curso não diz exatamente em que situações vamos utilizar, etc.,
+ * mas esclarece que quando precisamos "dar um clique" num botão, ou seja,...
+ * quando precisamos apenas de um recurso, podemos pensar em utilizar a classe anônima.
+ * Acho que também é um conceito que vai se solidificar um pouco mais na frente. 
+ * Esta interface está sendo utilizada na classe anônima,...continua lá...  
+ */
+
+package Anônima;
+
+public interface Caminhar {
+	
+	public void avançar () {	
+	}
+}
