50dd939aad83b6f6e65ba1285c3ecd4e2877fefb
Cláudio Leal <claudiolealti@gmail.com>
2020-06-01 21:33:41 -0300

    Versão 18

---
+++ src/Aninhado/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Classe main criada. Chamará os recursos da classe Lampada. *  
+ */
+
+package Aninhado;
+
+public class Main {	
+	public static void main (String [] args) {
+		// Classe "Lampada" que se tornou um tipo de dado.
+		// Objeto ou variável "lampada" criada por este tipo de dado.
+		// Objeto "lampada" recebe uma instância da classe Lampada. 
+		Lampada lampada = new Lampada();
+	}
+}
