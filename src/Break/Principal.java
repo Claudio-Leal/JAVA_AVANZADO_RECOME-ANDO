e60b930685eb28dcd117d6e274c92158d2c751ef
Cláudio Leal <claudiolealti@gmail.com>
2020-06-02 08:55:31 -0300

    Versão 1 - Break and continue

---
+++ src/Break/Principal.java
@@ -0,0 +1,17 @@
+/*
+ * Informações importantes sobre as instruções break e continue. 
+ */
+
+
+package Break;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int numero = 0;
+		while (numero < 20) {
+			numero ++;
+			System.out.println(numero);							
+		}
+	}
+}
