dcb4a8f71b6122c1eb63eba59412237f4f7d6b41
Cláudio Leal <claudiolealti@gmail.com>
2020-06-02 09:15:31 -0300

    Versão 4 - Já com o "continue" inserido

--- src/Break/Principal.java
+++ src/Break/Principal.java
@@ -1,5 +1,7 @@
 /*
- * Informações importantes sobre as instruções break e continue. 
+ * Informações importantes sobre as instruções break e continue: 
+ * A instrução break interrompe a iteração, ou seja, apenas um ciclo ocorreu.
+ * A instrução continue "simplesmente" diz,..continue a executar o ciclo. 
  */
 
 
@@ -8,6 +10,34 @@ package Break;
 public class Principal {
 
 	public static void main(String[] args) {
+		int i;
+		
+		// Break
+		for (i = 0 ; i < 10 ; i++) {
+			System.out.println("1");
+			System.out.println("2");
+			break;
+		}
+		// Continue
+		for (i = 0 ; i < 10 ; i++) {
+			System.out.println("3");
+			System.out.println("4");
+			continue;
+		}		
+	}
+}
+		
+
+		
+		
+
+		
+		
+		
+
+/*
+//----------------------------------------------------------BREAK INÍCIO		
+		// Com as instruções para parar. 
 		int numero = 0;
 		while (numero < 20) {			
 			System.out.println(numero);
@@ -19,5 +49,6 @@ public class Principal {
 			// Posição correta para a instrução de incremento. 
 			numero ++;
 		}
-	}
-}
+//----------------------------------------------------------BREAK FIM
+*/
+ 
