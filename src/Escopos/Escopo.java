6528c19dd2b316706046113b17d584cf60fa254c
Cláudio Leal <claudiolealti@gmail.com>
2020-06-02 16:58:41 -0300

    Versão 1 - Escopo da classe

---
+++ src/Escopos/Escopo.java
@@ -0,0 +1,25 @@
+/*
+ * A ideia aqui é só de dar "nomes aos bois". 
+ * O bloco que temos abaixo se chama Escopo da classe. 
+ */
+
+
+package Escopos;
+
+//--------------------------------Início do escopo
+public class Escopo {
+	
+	// Atributos (variáveis)
+	int numero;
+	String nome;
+	boolean booleano;
+	
+	// Método 1
+	public void metodo1 ( ) {		
+	}
+	
+	// Método 2
+	public void metodo2 ( ) {		
+	}	
+}
+//--------------------------------Fim do escopo
