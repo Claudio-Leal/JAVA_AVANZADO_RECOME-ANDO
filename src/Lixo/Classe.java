37f3704d6a1f5209d28bfd5b49035a4ab1020498
Cláudio Leal <claudiolealti@gmail.com>
2020-06-02 16:48:13 -0300

    Versão 3 e final - coleta de lixo - observações em pull request

--- src/Lixo/Classe.java
+++ src/Lixo/Classe.java
@@ -7,25 +7,26 @@ public class Classe {
 		// Super classe Object criando um objeto chamado "objeto". 
 		// = new Object (), instanciando a super classe Object, criando um ojbeto. 
 		// Podemos dizer que assim "se cria um objeto".
-		Object objeto1 = new Object();
+		Object objetoInicial = new Object();
 		
 		// Igualando a null, estaremos liberando espaço na memória para o objeto declarado.
 		// Podemos dizer que assim "se libera um objeto". 
 		// Este é justamente um recurso chamado "coleta de lixo", ou seja, de "liberação de memória", para que 
-		objeto1 = null;
-		//------------------------------FIM
-		
-		
+		objetoInicial = null;
+		//------------------------------FIM		
 		
 		
 		
 		//------------------------------INÍCIO
 		// Aqui também temos a criação de um objeto, chamado "objeto2", com a utilização da classe Main.
 		// Aqui se cria um objeto.
-		Main objeto2 = new Main();
+		Main objetoFinal = new Main();
 		
 		// Aqui se libera um objeto. 
-		objeto2 = null;
+		// System.gc () chama o método finalize,...mas não mostra. 
+		// São as características da "limpeza de lixo" do java. 
+		// Mas ele imprime a palavra final existente no método finalize. 
+		objetoFinal = null;
 		System.gc();
 		//------------------------------FIM
 		
